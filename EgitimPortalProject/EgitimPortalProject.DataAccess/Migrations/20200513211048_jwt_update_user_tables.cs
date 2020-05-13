41e9b68c39b45b5d8aa28e30f203ba49cfdea48a
Vacip <vacipderici@gmail.com>
2020-05-14 00:11:57 +0300

    jwt migration eklendi düzenlendi.

---
+++ EgitimPortalProject/EgitimPortalProject.DataAccess/Migrations/20200513211048_jwt_update_user_tables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EgitimPortalProject.DataAccess.Migrations
+{
+    public partial class jwt_update_user_tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
